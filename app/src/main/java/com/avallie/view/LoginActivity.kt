c89ba5a82a3fc24e64571684d38e6ac108cf0d9f
Vinicius Lima <kiddlima@outlook.com>
2019-03-20 11:17:25 -0300

    Inicio da tela de login

---
+++ app/src/main/java/com/avallie/view/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.avallie.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avallie.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
