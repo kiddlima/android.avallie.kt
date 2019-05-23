e19f5ba9bea40d000c44e63e29acf144d6ded7a4
Vinicius Lima <kiddlima@outlook.com>
2019-05-23 10:46:53 -0300

    Databinding

--- app/src/main/java/com/avallie/view/BudgetProductDetailActivity.kt
+++ app/src/main/java/com/avallie/view/BudgetProductDetailActivity.kt
@@ -2,7 +2,7 @@ package com.avallie.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.avallie.R
+import com.avallie.databinding.ActivityBudgetProductDetailBinding
 import com.avallie.model.Budget
 import com.avallie.model.SelectedProduct
 
@@ -10,29 +10,34 @@ class BudgetProductDetailActivity : AppCompatActivity() {
 
     val SELECTED_PRODUCT = "selected-product"
 
-    private val selectedProduct: SelectedProduct by lazy {
+    val selectedProduct: SelectedProduct by lazy {
         intent.getSerializableExtra(SELECTED_PRODUCT) as SelectedProduct
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_budget_product_detail)
+        val binding = ActivityBudgetProductDetailBinding.inflate(layoutInflater)
+//        mockBudgets()
 
+//        binding.model = this
 
-
-        mockBudgets()
+        setContentView(binding.root)
     }
 
     private fun mockBudgets() {
         for (i in 0..4) {
-              selectedProduct.budgets.add(Budget("Balarotti",
+            selectedProduct.budgets.add(
+                Budget(
+                    "Balarotti",
                     "Em até 12x no cartão sem juros",
                     "Em até 10 dias úteis após pagamento",
                     "Pronta entrega",
                     2599.0,
                     2399.0,
                     0.5,
-                    75.0))
+                    75.0
+                )
+            )
         }
     }
 }
